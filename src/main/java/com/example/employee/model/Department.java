c2265d9ed92674e2b22be2214a0c69f1ca67d2d4
Huncho Jack <meniman98@gmail.com>
2022-09-23 12:36:15 +0200

    added models

---
+++ src/main/java/com/example/employee/model/Department.java
@@ -0,0 +1,46 @@
+package com.example.employee.model;
+
+public abstract class Department {
+
+    abstract int getNumberOfEmployees();
+    abstract String getDepartmentDescription();
+
+}
+
+sealed class Engineering extends Department permits Development, Testing, Data  {
+
+    @Override
+    public int getNumberOfEmployees() {
+        return 200;
+    }
+
+    @Override
+    public String getDepartmentDescription() {
+        return "At engineering, amazing products are built";
+    }
+
+    public static void main(String[] args) {
+        Engineering engineering = new Engineering();
+    }
+}
+
+final class Development extends Engineering {}
+final class Testing extends Engineering {}
+final class Data extends Engineering {}
+
+sealed class Sales extends Department permits LeadGeneration, Qualifier, Closing, Upselling {
+    @Override
+    int getNumberOfEmployees() {
+        return 300;
+    }
+
+    @Override
+    String getDepartmentDescription() {
+        return "At sales, the amazing products that are built are sold here";
+    }
+}
+
+final class LeadGeneration extends Sales{}
+final class Qualifier extends Sales {}
+final class Closing extends Sales{}
+final class Upselling extends Sales {}
---
+++ src/main/java/com/example/employee/model/Employee.java
@@ -0,0 +1,17 @@
+package com.example.employee.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+import java.time.LocalDate;
+
+public record Employee(@Id
+                       @GeneratedValue(strategy = GenerationType.AUTO)
+                       Long id,
+                       String name, LocalDate dateOfBirth, Department department) {
+}
+
+
+
